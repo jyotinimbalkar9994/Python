44239cfbccb26856f88cd4b80bae55d4553880cd
jyotinimbalkar <jyotinimbalkar999@gmail.com>
2026-07-29 18:59:46 +0530

    first commit

---
+++ chapter python firstprogram/First.py
@@ -0,0 +1,25 @@
+#print("Hello world")
+# syntax to print string in python ---print("     ")
+print("Jyoti Nimbalakar")
+# to comment out in python use # before the line
+
+print("Hello World") 
+#  This is how we add a comment in python
+
+# This is a variable
+a = 5
+print(a) 
+
+# This is a variable
+Instructor = 'jyoti Nimbalkar'
+print("Python by", Instructor, sep='-')
+
+# python as a calculator
+print("5+3")
+print(5+3)
+
+b = 10
+c = 20
+print(b+c)
+print(c/b)
+print(c*b)
